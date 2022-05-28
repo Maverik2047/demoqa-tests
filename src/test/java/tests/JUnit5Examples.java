4c5e426f770c44216016205b353dc7252d24b8f2
Viacheslav <maverik2047@yandex.ru>
2022-05-28 17:37:00 +0300

    added junit5 examples

---
+++ src/test/java/tests/JUnit5Examples.java
@@ -0,0 +1,12 @@
+package tests;
+
+import org.junit.jupiter.api.Test;
+
+public class JUnit5Examples {
+    @Test
+    void firstTest(){
+        System.out.println("My first test!!");
+    }
+
+
+}
